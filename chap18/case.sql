3c73abfe74adb09b8964ecdf2eaec7a6ee820341
面川泰明 <omokawa@senk-inc.co.jp>
2020-03-10 10:07:27 +0900

    Complete case element

--- chap18/case.sql
+++ chap18/case.sql
@@ -1,6 +1,27 @@
+-- correct
 case
   when 1 = 1
   then NULL
   else cast
 (null as integer)
 end
+
+-- same as correct
+case
+  when 1 = 1
+  then cast
+(null as integer)
+  else NULL
+END
+
+-- incorrect, not defined data type
+case
+  when 1 = 1
+  then NULL
+  else NULL
+end
+
+-- depends on database implementation
+cast
+(case when 1 = 1 then null else null
+end as integer)
