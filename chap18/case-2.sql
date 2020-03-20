ce5625d7f07f964d921e4e3639b2c0d62d0f4df0
面川泰明 <omokawa@senk-inc.co.jp>
2020-03-20 09:44:14 +0900

    Check PersonnelSkills

--- chap18/case-2.sql
+++ chap18/case-2.sql
@@ -21,3 +21,19 @@ create table PersonnelSkills (
   primary_skill_flg char(1) not null constraint primary_skill_given check (primary_skill_flg in ('Y', 'N')),
   primary key (esp_id, skill_code)
 );
+
+-- Show personSkill
+select
+  esp_id,
+  case
+    when count(*) = 1 then max(skill_code)
+    else max(
+      case
+        when primary_skill_flg = 'Y' then skill_code
+      end
+    )
+  end as main_skill
+from
+  PersonnelSkills
+group by
+  esp_id;
