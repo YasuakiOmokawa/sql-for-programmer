cb1e8045acb1e123a8e82b636a93c363f8ecf30b
面川泰明 <omokawa@senk-inc.co.jp>
2020-04-04 13:27:01 +0900

    Define selco ogirinal version

--- chap18/case-2-1.sql
+++ chap18/case-2-1.sql
@@ -1,16 +1,23 @@
 -- セルコオリジナルバージョン
-create table　Foobar_DDL
+CREATE table FOOBAR_DDL
 (
-	a char(1) check (a in ('T',　'F')),
-	b char(1) check (b in ('T',　'F')),
-	constraint implication_example
-		check (not (A = 'T')　or (B = 'T'))
+  A CHAR(1) CHECK (A IN ('T', 'F')),
+  B CHAR(1) CHECK (B IN ('T', 'F')),
+  CONSTRAINT IMPLICATION_EXAMPLE_2 CHECK
+  (CASE
+    WHEN A = 'T' THEN
+    CASE
+      WHEN B = 'T' THEN 1
+      ELSE 0
+    END
+    ELSE 1
+  END = 1)
 );
 
 -- インサート
 insert into foobar_ddl values ('T', 'T');
 insert into foobar_ddl values ('T', 'F'); -- 制約変換で失敗
-insert into foobar_ddl values ('T', NULL);
+insert into foobar_ddl values ('T', NULL); -- 制約変換で失敗
 insert into foobar_ddl values ('F', 'T');
 insert into foobar_ddl values ('F', 'F');
 insert into foobar_ddl values ('F', NULL);
