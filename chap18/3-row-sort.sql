646d2eca55ab8ae8448c74574cc52f844083edc3
面川泰明 <omokawa@senk-inc.co.jp>
2020-04-14 10:40:49 +0900

    Update row set

--- chap18/3-row-sort.sql
+++ chap18/3-row-sort.sql
@@ -14,17 +14,26 @@ insert into
 values
   (1, 'A', 'B', 'C', 'D', 'E')
   ,(2, 'E', 'D', 'C', 'B', 'A')
+  ,(3, 'D', 'E', 'C', 'B', 'A')
 ;
 select * from foobar_18_3;
-
 -- Swap(c1, c2) and Swap(c4, c5);
 update
   foobar_18_3
 set
   c1 = case when c1 <= c2 then c1 else c2 end,
-  c2 = case when c1 <= c2 then c2 else c1 end,
-  c4 = case when c4 <= c5 then c4 else c5 end,
-  c5 = case when c4 <= c5 then c5 else c4 end
+  c2 = case when c1 <= c2 then c2 else c1 end
+--   c4 = case when c4 <= c5 then c4 else c5 end,
+--   c5 = case when c4 <= c5 then c5 else c4 end
 where
   c4 > c5 or c1 > c2
+;
+
+-- swap(c1, c2)
+update
+  foobar_18_3
+set
+  c1 = c2, c2 = c1
+where
+  c1 > c2
 ;
