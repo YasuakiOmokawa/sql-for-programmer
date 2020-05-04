156c61e80b1c35156967c9eabdc925c8523e1127
面川泰明 <omokawa@senk-inc.co.jp>
2020-05-04 09:59:22 +0900

    Add calc single sql on several condition

--- deeper-coach-sql/supreme-case-expression.sql
+++ deeper-coach-sql/supreme-case-expression.sql
@@ -36,3 +36,50 @@ select
 from poptbl
 group by district
 ;
+
+-- Calculate single query on several condition
+DROP TABLE poptbl2;
+CREATE TABLE PopTbl2 (
+  pref_name VARCHAR(32),
+  sex char(1) not null,
+  population INTEGER NOT null,
+  primary key(pref_name, sex)
+);
+
+delete from poptbl2;
+select * from poptbl2;
+INSERT INTO
+  PopTbl2
+VALUES
+  ('徳島', '1', 60)
+  ,('徳島', '2', 40)
+  ,('香川', '1', 100)
+  ,('香川', '2', 100)
+  ,('愛媛', '1', 100)
+  ,('愛媛', '2', 50)
+  ,('高知', '1', 100)
+  ,('高知', '2', 100)
+  ,('福岡', '1', 100)
+  ,('福岡', '2', 200)
+  ,('佐賀', '1', 20)
+  ,('佐賀', '2', 80)
+  ,('長崎', '1', 150)
+  ,('長崎', '2', 50)
+  ,('東京', '1', 210)
+  ,('東京', '2', 190)
+  ,('群馬', '1', 240)
+  ,('群馬', '2', 260)
+;
+
+-- summarize
+select
+  pref_name
+  -- population of man
+  ,sum(case when sex = '1' then population else 0 end) as cnt_m
+  -- case of female
+  ,sum(case when sex = '2' then population else 0 end) as cnt_f
+from
+  poptbl2
+group by
+  pref_name
+;
