c76f72302a8c04f2cb17d51201f393aac82f7e77
面川泰明 <omokawa@senk-inc.co.jp>
2020-05-08 09:48:47 +0900

    Create new table to check conditional update

--- deeper-coach-sql/supreme-case-expression.sql
+++ deeper-coach-sql/supreme-case-expression.sql
@@ -83,3 +83,16 @@ from
 group by
   pref_name
 ;
+
+-- CHECK CONDITION IF test TABLE
+CREATE TABLE TestSal
+(sex char(1),
+salary integer,
+  CONSTRAINT check_salary CHECK
+    ( CASE
+        WHEN sex = '2'
+        THEN CASE
+          WHEN salary <= 200000
+          THEN 1 ELSE 0 END
+          ELSE 1 END = 1))
+;
