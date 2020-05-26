e5ada59d6b7e15ba805ba28f4e5343a3b4162a21
面川泰明 <omokawa@senk-inc.co.jp>
2020-05-26 09:16:58 +0900

    Add grouping match with case statement

--- deeper-coach-sql/supreme-case-expression.sql
+++ deeper-coach-sql/supreme-case-expression.sql
@@ -194,3 +194,26 @@ select cm.course_name,
     else '×' end as "8月" 
 from coursemaster cm
 ;
+
+-- CASE式の中で集約関数を使う
+CREATE TABLE StudentClub
+(std_id  INTEGER,
+ club_id INTEGER,
+ club_name VARCHAR(32),
+ main_club_flg CHAR(1),
+ PRIMARY KEY (std_id, club_id));
+INSERT INTO StudentClub VALUES(100, 1, '野球',        'Y');
+INSERT INTO StudentClub VALUES(100, 2, '吹奏楽',      'N');
+INSERT INTO StudentClub VALUES(200, 2, '吹奏楽',      'N');
+INSERT INTO StudentClub VALUES(200, 3, 'バドミントン','Y');
+INSERT INTO StudentClub VALUES(200, 4, 'サッカー',    'N');
+INSERT INTO StudentClub VALUES(300, 4, 'サッカー',    'N');
+INSERT INTO StudentClub VALUES(400, 5, '水泳',        'N');
+INSERT INTO StudentClub VALUES(500, 6, '囲碁',        'N');
+select * from studentclub s ;
+-- grouping match in case statement
+select std_id,
+  case when count(*) = 1 -- only have primary club
+    when max(club_id)
+  else max(case when main_club_flg = 'Y')
+
