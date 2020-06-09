17c3492bf934daa60c357a402201858fb8a722c8
面川泰明 <omokawa@senk-inc.co.jp>
2020-06-09 09:54:41 +0900

    Coding correct answer

--- deeper-coach-sql/supreme-case-expression.sql
+++ deeper-coach-sql/supreme-case-expression.sql
@@ -289,7 +289,18 @@ group by
 order by
     sex
 ;
-
+-- 1-2, ver.mick
+select
+  sex,
+  sum(population) as total,
+  sum(case when pref_name = '徳島' then population else 0 end) as col_1 -> tokushima,
+  sum(case when pref_name = '香川' then population else 0 end) as col_1 -> kagawa,
+  sum(case when pref_name = '愛媛' then population else 0 end) as col_1 -> ehime,
+  sum(case when pref_name = '高知' then population else 0 end) as col_1 -> kouchi,
+  sum(case when pref_name in ('徳島', '香川', '愛媛', '高知') then population else 0 end) as shikoku
+from
+  poptbl2
+group by sex;
 
 
 
