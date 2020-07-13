71a02d577551bf84532c297072eb662f3a1ed444
面川泰明 <omokawa@senk-inc.co.jp>
2020-07-13 09:40:15 +0900

    Add test table

---
+++ deeper-coach-sql/think-null.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Class_A
+(name CHAR(16) PRIMARY KEY,
+ age INTEGER,
+ city CHAR(16));
+
+CREATE TABLE Class_B
+(name CHAR(16) PRIMARY KEY,
+ age INTEGER,
+ city CHAR(16));
+
+INSERT INTO class_a VALUES('ブラウン',  22, '東京');
+INSERT INTO class_a VALUES('ラリー',  19, '埼玉');
+INSERT INTO class_a VALUES('ボギー',  21, '千葉');
+
+INSERT INTO class_b VALUES('斎藤',  22, '東京');
+INSERT INTO class_b VALUES('田尻',  23, '東京');
+
+
