a2df51df2b868502ec2b3b9f8fca9270f73eaefb
面川泰明 <omokawa@senk-inc.co.jp>
2020-07-15 09:30:10 +0900

    Write sample to check null damage

--- deeper-coach-sql/think-null.sql
+++ deeper-coach-sql/think-null.sql
@@ -14,5 +14,24 @@ INSERT INTO class_a VALUES('ボギー',  21, '千葉');
 
 INSERT INTO class_b VALUES('斎藤',  22, '東京');
 INSERT INTO class_b VALUES('田尻',  23, '東京');
+INSERT INTO class_b VALUES('山田',  20, '東京');
+INSERT INTO class_b VALUES('和泉',  18, '千葉');
+INSERT INTO class_b VALUES('武田',  20, '千葉');
+INSERT INTO class_b VALUES('石川',  19, '神奈川');
 
+select * from class_a;
+select * from class_b;
 
+-- Choose student youngest than class_b in Tokyo city
+select
+  *
+from 
+  class_a
+where age < all(select 
+                  age
+                from 
+                  class_b 
+                where 
+                  city = '東京');
+
+                
